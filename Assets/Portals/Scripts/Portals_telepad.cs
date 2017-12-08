3963fc75fd35de3bf55df04257fb60dad4f3871c
opacut <oliver.pacut@gmail.com>
2017-12-08 16:21:53 +0100

    Added Telepads

---
+++ Assets/Portals/Scripts/Portals_telepad.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portals_telepad : MonoBehaviour
+{
+    public Portals_telepad[] destinations;
+    public string tags = "|Player|";
+    private bool active = true;
+    private int cooldown = 0;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (!active)
+        {
+            cooldown++;
+        }
+        if (cooldown >= 60)
+        {
+            active = true;
+            cooldown = 0;
+        }
+	}
+
+    public void Deactivate()
+    {
+        active = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(active&&tags.Contains(string.Format("|{0}|", other.tag)))
+        {
+            int r = Random.Range(0, destinations.Length - 1);
+            destinations[r].Deactivate();
+            other.transform.position = destinations[r].GetComponent<Collider>().transform.position;
+            other.transform.rotation = destinations[r].transform.rotation;
+        }
+    }
+}
