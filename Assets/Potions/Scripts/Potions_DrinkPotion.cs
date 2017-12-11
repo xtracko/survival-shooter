5eff2b5f0ef8e9de903fe61b6549feb4e9c47bd5
kikinna <zakopcanova.k@gmail.com>
2017-12-11 23:05:33 +0100

    New game mecanics BUT
    * changed PlayerShooting in PlayerHealth to Potions_PlayerShooting, thus needs new copy -- Potions_PlayerHealth

---
+++ Assets/Potions/Scripts/Potions_DrinkPotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class Potions_DrinkPotion : MonoBehaviour
+    {
+
+        GameObject player;
+        Potions_PlayerHealth playerHealth;
+        
+
+        void Awake()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            playerHealth = player.GetComponent<Potions_PlayerHealth>();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject == player)
+            {
+                playerHealth.Heal();
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Potions/Scripts/Potions_PotionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class Potions_PotionManager : MonoBehaviour
+    {
+        public GameObject potion;
+
+        public static Potions_PotionManager instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+        }
+
+        public void CreatePotion(Vector3 position)
+        {
+            Instantiate(potion, position, Quaternion.identity);
+        }
+    }
+}
